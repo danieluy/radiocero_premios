49fc4590bd36bb4387ecb3f200db01f02ed237a4
danielsosauy <danielsosa.dev@gmail.com>
2017-09-06 11:28:18 -0300

    Added permissions description (temporarily)

--- react-client/src/components/drawer/Drawer.js
+++ react-client/src/components/drawer/Drawer.js
@@ -9,6 +9,7 @@ import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import ListItem from 'material-ui/List/ListItem';
 import Avatar from 'material-ui/Avatar';
+import Subheader from 'material-ui/Subheader';
 
 import { AcountsIcon, WinnersIcon, PrizesIcon, HomeIcon } from '../../assets/icons'
 
@@ -49,47 +50,46 @@ class DrawerCustom extends Component {
           className="drawer"
           width={300}
         >
-          <div className="drawer-header">
-            <img className="drawer-header-icon" src="../assets/img/icon_192.png" />
-            Radiocero Premios v2
-          </div>
           <Link to="/v2/" onClick={this.setActiveLink.bind(this, '/v2/')}>
-            <MenuItem
-              primaryText="Inicio"
-              leftIcon={<HomeIcon className="drawer-link-icon" />}
-              style={this.setConditionalStyle("/v2/")}
-            />
+            <div className="drawer-header" style={this.setConditionalStyle("/v2/")}>
+              <img className="drawer-header-icon" src="../assets/img/icon_192.png" />
+              <div className="drawer-header-text">Radiocero Premios v2</div>
+            </div>
           </Link>
           {this.state.loggedUser ?
-            <Link to="/v2/winners" onClick={this.setActiveLink.bind(this, '/v2/winners')}>
-              <MenuItem
-                primaryText="Ganadores"
-                leftIcon={<WinnersIcon className="drawer-link-icon" />}
-                style={this.setConditionalStyle("/v2/winners")}
-              />
-            </Link>
-            : null
-          }
-          {this.state.loggedUser ?
-            <Link to="/v2/prizes" onClick={this.setActiveLink.bind(this, '/v2/prizes')}>
-              <MenuItem
-                primaryText="Premios"
-                leftIcon={<PrizesIcon className="drawer-link-icon" />}
-                style={this.setConditionalStyle("/v2/prizes")}
-              />
-            </Link>
+            <div>
+              <Subheader>Usuarios</Subheader>
+              <Link to="/v2/winners" onClick={this.setActiveLink.bind(this, '/v2/winners')}>
+                <MenuItem
+                  primaryText="Ganadores"
+                  leftIcon={<WinnersIcon className="drawer-link-icon" />}
+                  style={this.setConditionalStyle("/v2/winners")}
+                />
+              </Link>
+              <Link to="/v2/prizes" onClick={this.setActiveLink.bind(this, '/v2/prizes')}>
+                <MenuItem
+                  primaryText="Premios"
+                  leftIcon={<PrizesIcon className="drawer-link-icon" />}
+                  style={this.setConditionalStyle("/v2/prizes")}
+                />
+              </Link>
+            </div>
             : null
           }
           {(this.state.loggedUser && this.state.loggedUser.role === 'admin') ?
-            <Link to="/v2/users" onClick={this.setActiveLink.bind(this, '/v2/users')}>
-              <MenuItem
-                primaryText="Usuarios"
-                leftIcon={<AcountsIcon className="drawer-link-icon" />}
-                style={this.setConditionalStyle("/v2/users")}
-              />
-            </Link>
+            <div>
+              <Subheader>Administradores</Subheader>
+              <Link to="/v2/users" onClick={this.setActiveLink.bind(this, '/v2/users')}>
+                <MenuItem
+                  primaryText="Usuarios"
+                  leftIcon={<AcountsIcon className="drawer-link-icon" />}
+                  style={this.setConditionalStyle("/v2/users")}
+                />
+              </Link>
+            </div>
             : null
           }
+
         </Drawer>
       </div>
     );
@@ -97,3 +97,4 @@ class DrawerCustom extends Component {
 }
 
 export default DrawerCustom;
+
--- react-client/src/components/home/Home.js
+++ react-client/src/components/home/Home.js
@@ -1,13 +1,96 @@
-import React, { Component } from 'react';
+import React, { Component, PureComponent } from 'react';
+
+import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table';
+import Subheader from 'material-ui/Subheader';
 
 class Home extends Component {
- render() {
-   return (
-     <div className="home-root">
-       <img src="/assets/img/icon_192.png" alt="Logotipo de Radiocero" />
-     </div>
-   );
- }
+  render() {
+    return (
+      <div>
+        <div className="home-root">
+          <img src="/assets/img/icon_192.png" alt="Logotipo de Radiocero" />
+        </div>
+        <PermissionsDescription />
+      </div>
+    );
+  }
 }
 
 export default Home;
+
+class PermissionsDescription extends PureComponent {
+  constructor() {
+    super()
+    // this.permissions = [
+    //   { 'Ver Usuario': 'Administrador' },
+    //   { 'Crear Usuario': 'Administrador' },
+    //   { 'Editar Usuario': 'Administrador' },
+    //   { 'Eliminar Usuario': 'Administrador' }
+    // ]
+
+    this.permissions = [
+      {
+        'Usuarios': {
+          create: 'Administrador',
+          read: 'Administrador',
+          update: 'Administrador',
+          delete: 'Administrador'
+        }
+      }, {
+        'Premios': {
+          create: 'Usuarios',
+          read: 'Usuarios',
+          update: 'Usuarios',
+          delete: 'Administrador'
+        }
+      }, {
+        'Ganadores': {
+          create: 'Usuarios',
+          read: 'Usuarios',
+          update: 'Usuarios',
+          delete: 'Usuarios'
+        }
+      }
+    ]
+  }
+  render() {
+    return (
+      <div>
+        <Subheader>Descripcíon de Permisos</Subheader>
+        <p style={{ padding: '0 16px', color: "#888" }}>
+          <small>Acceso a los principales tipos de datos por tipo de usuario</small>
+        </p>
+        <Table>
+          <TableHeader
+            displaySelectAll={false}
+            adjustForCheckbox={false}
+          >
+            <TableRow>
+              <TableHeaderColumn></TableHeaderColumn>
+              <TableHeaderColumn>Crear</TableHeaderColumn>
+              <TableHeaderColumn>Leer</TableHeaderColumn>
+              <TableHeaderColumn>Editar</TableHeaderColumn>
+              <TableHeaderColumn>Eliminar</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          <TableBody
+            displayRowCheckbox={false}
+          >
+            {this.permissions.map((row, i) => {
+              const key = Object.keys(row)[0]
+              return (
+                <TableRow key={i}>
+                  <TableRowColumn>{key}</TableRowColumn>
+                  <TableRowColumn>{row[key].create}</TableRowColumn>
+                  <TableRowColumn>{row[key].read}</TableRowColumn>
+                  <TableRowColumn>{row[key].update}</TableRowColumn>
+                  <TableRowColumn>{row[key].delete}</TableRowColumn>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+      </div>
+    )
+  }
+}
